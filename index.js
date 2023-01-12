7103ef7418e6ce0c48de07187339eb7f00e0c16c
ashlee.larrick <ashleemareelarrick323@gmail.com>
2023-01-12 12:59:40 -0700

    Help

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(street) {
+    return Math.abs(street - 42)
+}
+
+function distanceFromHqInFeet(feet) {
+    distanceFromHqInBlocks(blocks)
+}
+
+function distanceTravelledInFeet(start, destination) {
+
+}
+
+function calculatesFarePrice(start, destination) {
+}
