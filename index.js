f9afe87f2cdc5cee1a662cf93cc28722fd1a4ed3
ashlee.larrick <ashleemareelarrick323@gmail.com>
2023-01-16 10:43:31 -0700

    Finished Lab

--- index.js
+++ index.js
@@ -2,13 +2,26 @@ function distanceFromHqInBlocks(street) {
     return Math.abs(street - 42)
 }
 
-function distanceFromHqInFeet(feet) {
-    distanceFromHqInBlocks(blocks)
+function distanceFromHqInFeet(street) {
+    return distanceFromHqInBlocks(street) * 264
 }
 
 function distanceTravelledInFeet(start, destination) {
-
+    return Math.abs(destination - start) * 264
 }
 
 function calculatesFarePrice(start, destination) {
+    let number_of_feet = distanceTravelledInFeet(start, destination)
+    if(number_of_feet <= 400) {
+        return 0
+    }
+    else if(number_of_feet > 400 && number_of_feet <= 2000) {
+        return (number_of_feet - 400) * .02
+    }
+    else if (number_of_feet > 2000 && number_of_feet < 2500) {
+        return 25
+    }
+    else {
+        return 'cannot travel that far'
+    }
 }
